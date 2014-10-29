5f936955366ac2e19fa604cd639777ea62442f69
Poorak Kashyap <poorakk@gmail.com>
2014-10-28 20:44:59 -0700

    isNumber

---
+++ src/main/java/com/poorak/Arrays/ValidNumber.java
@@ -0,0 +1,87 @@
+package com.poorak.Arrays;
+
+/**
+ * Validate if a given string is numeric.
+
+Some examples:
+"0" => true
+" 0.1 " => true
+"abc" => false
+"1 a" => false
+"2e10" => true
+Note: It is intended for the problem statement to be ambiguous. 
+You should gather all requirements up front before implementing one.
+
+ * @author poorak
+ *
+ */
+public class ValidNumber {
+	
+     
+     
+    boolean valid(String str){
+    	char[] s = str.toCharArray();
+        int i=0;
+        boolean e =false; // check if 'e' exists
+        boolean dot=false; // check if '.' exists
+        boolean dig =false;
+         
+        while (i<s.length-1){
+            if (i==0){ // a number can start with +, -, .
+                if (s[i]<'0' || s[i]>'9'){ // if is 0-9 continue
+                    if (s[i]=='+' || s[i]=='-' || s[i]=='.'|| s[i]==' '){
+                        if (s.length==1){return false;} // only +, - , . is not a number
+                        if (s[i]=='.'){dot=true;}
+                    }
+                    else {return false;}
+                }else{dig=true;}
+                i++;continue;
+            }
+            if (i>0){
+                switch (s[i]){
+                    case 'e': // e cannot follow +,-
+                        if ( e==false && s[i-1]!='+' && s[i-1]!='-' && dig && i!=s.length-1) {
+                            e = true;
+                        }else{
+                            return false;
+                        }
+                        break;
+                   case 'E': // e cannot follow +,-
+                        if ( e==false && s[i-1]!='+' && s[i-1]!='-' && dig && i!=s.length-1) {
+                            e = true;
+                        }else{
+                            return false;
+                        }
+                        break;
+                   case '+': // + can only occur before e
+                        if (s[i-1]=='e' || s[i-1]=='E'){}else{return false;}
+                        break;
+                   case '-': // - can only occur before e
+                        if (s[i-1]=='e' || s[i-1]=='E'){}else{return false;}
+                        break;
+                   case '.': // . can only occur once and cannot occure after e
+                        if (dot==false && e==false){dot=true;}else{return false;}
+                        break;
+                   default:  // only 0-9 can be valid numbers
+                        if (s[i]<'0'||s[i]>'9'){return false;}
+                        else{dig = true;}
+                        break;
+            } 
+                i++;continue;
+            }
+        }
+         
+        //last dig can only be 0-9, or ., when it is . there is no . and e before
+        if (s[s.length-1]>='0' && s[s.length-1]<='9'){return true;}
+        if (s[s.length-1]=='.' && s.length ==1) {return false;}       
+        if (s[s.length-1]=='.' && !dot && !e && s[s.length-2]>='0' && s[s.length-1]<='9') {return true;}       
+        return false;
+    }
+    boolean isNumber(String s) {
+  
+        String s1 = s.trim(); 
+        if (s1.length()==0){return false;} // if null string, return false;
+        return valid(s1);
+    }
+
+}
---
+++ src/test/java/com/poorak/Arrays/ValidNumberTest.java
@@ -0,0 +1,116 @@
+package com.poorak.Arrays;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ValidNumberTest {
+
+	private ValidNumber validNumber = new ValidNumber();
+	@Before
+	public void setUp() throws Exception {
+	} 
+
+	@Test
+	public final void test01() {
+		String s = "0.1";
+		assertTrue(validNumber.isNumber(s));
+	}
+	@Test
+	public final void test0() {
+		String s = "0";
+		assertTrue(validNumber.isNumber(s));
+	}
+	@Test
+	public final void testabc() {
+		String s = "abc";
+		assertFalse(validNumber.isNumber(s));
+	}
+	@Test
+	public final void test1a() {
+		String s = "1 a";
+		assertFalse(validNumber.isNumber(s));
+	}
+	@Test
+	public final void test2e10() {
+		String s = "2e10";
+		assertTrue(validNumber.isNumber(s));
+	}
+	@Test
+	public final void testee10() {
+		String s = "ee10";
+		assertFalse(validNumber.isNumber(s));
+	}
+	@Test
+	public final void test2e1e() {
+		String s = "2e1e";
+		assertFalse(validNumber.isNumber(s));
+	}
+	@Test
+	public final void test2dot10() {
+		String s = "2.10";
+		assertTrue(validNumber.isNumber(s));
+	}
+	@Test
+	public final void test1space() {
+		String s = "1 ";
+		assertTrue(validNumber.isNumber(s));
+	}
+	@Test
+	public final void testspace() {
+		String s = " ";
+		assertFalse(validNumber.isNumber(s));
+	}
+	@Test
+	public final void test2space() {
+		String s = "1  ";
+		assertTrue(validNumber.isNumber(s));
+	}
+	@Test
+	public final void test1frontspace() {
+		String s = " 1  ";
+		assertTrue(validNumber.isNumber(s));
+	}
+	@Test
+	public final void test2frontspace() {
+		String s = "    1 5  6 ";
+		assertFalse(validNumber.isNumber(s));
+	}
+	@Test
+	public final void testdot1() {
+		String s = ".1";
+		assertTrue(validNumber.isNumber(s));
+	}
+	@Test
+	public final void test1dot() {
+		String s = "1.";
+		assertTrue(validNumber.isNumber(s));
+	}
+	@Test
+	public final void testminus1() {
+		String s = "-1";
+		assertTrue(validNumber.isNumber(s));
+	}
+	@Test
+	public final void testminusdot1() {
+		String s = "-.1";
+		assertTrue(validNumber.isNumber(s));
+	}
+	@Test
+	public final void testplus1() {
+		String s = "+.8";
+		assertTrue(validNumber.isNumber(s));
+	}
+	@Test
+	public final void test0e() {
+		String s = "0e";
+		assertFalse(validNumber.isNumber(s));
+	}
+	@Test
+	public final void testDot() {
+		String s = ".";
+		assertFalse(validNumber.isNumber(s));
+	}
+
+}
